bffada5ffa8dcc944cff8f13d0fcc8859158b95a
liutuo <lioutall@163.com>
2023-04-22 20:05:26 +0800

    Update pom.xml and MainVerticle.java

--- src/main/java/com/tollge/MainVerticle.java
+++ src/main/java/com/tollge/MainVerticle.java
@@ -11,7 +11,6 @@ import io.vertx.core.Promise;
 import io.vertx.core.impl.cpu.CpuCoreSensor;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
